5b929e91383bce9dcf6b1b7d7000a70eae4820f5
Diego Santillan <diego.santillan@soyait.mx>
2023-08-24 10:52:30 -0600

    CommitF

--- src/main/java/com/ait/mx/controller/Controller.java
+++ src/main/java/com/ait/mx/controller/Controller.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ait.mx.dto.MyResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class Controller {
 	
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+	
 	@GetMapping("/saludo")
 	public ResponseEntity<MyResponseDTO> getsaludo(){
+		logger.info("estamos probando este nuevo pa ver que pex");
 		return new ResponseEntity<>(new MyResponseDTO("SALUDO","OK"),HttpStatus.OK);
 	}
 
